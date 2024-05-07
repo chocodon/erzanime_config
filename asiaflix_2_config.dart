4798358ad377cc61826c84d8c4f5e3a5bab16e0b
chocodon <128836750+chocodon@users.noreply.github.com>
2024-05-07 22:44:23 +0700

    Create asiaflix_2_config.dart

---
+++ asiaflix_2_config.dart
@@ -0,0 +1,22 @@
+{
+  "ADS": [
+    {
+      "select_main_ads": "APPLOVIN-M",
+      "select_backup_ads": "",
+      "initialize_sdk": "XrQNR1fFZJoeJfN4DgkFqU9NJlg8F5TkJ1yBTc8CfS4EkdndXfkPXB7d8pkBm_gUrE7GrCU3DB7zXGJtqNmQ1n",
+      "initialize_sdk_backup": "",
+      "main_ads_banner": "ab88ecdc11d116da",
+      "backup_ads_banner": "",
+      "main_ads_interstitial": "5842bd9b258fbd8e",
+      "backup_ads_interstitial": "",
+      "interstitial_counter": "15",
+      "maintenance": "0",
+      "status_app": "0",
+      "link_redirect": "https://play.google.com/store/apps/details?id=com.devgpg.drama_tv",
+      "base_url": "https://dramacool.com.pa/",
+      "current_version": "1.1.1",
+      "main_ads_rewarded": "",
+      "rewarded_counter": 1
+    }
+  ]
+}
