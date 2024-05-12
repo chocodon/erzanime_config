19fa506a0e59e573fa1aefff440e88ae1aacb5f6
AWPdeveloper <122104464+AWPdeveloper@users.noreply.github.com>
2024-05-12 15:59:26 +0700

    Update asiaflix_2_config.dart

--- asiaflix_2_config.dart
+++ asiaflix_2_config.dart
@@ -14,7 +14,7 @@
       "status_app": "1",
       "link_redirect": "https://play.google.com/store/apps/details?id=com.drama.asiaflix_tv",
       "base_url": "https://dramacool.com.pa/",
-      "current_version": "1.1.1",
+      "current_version": "1.1.2",
       "main_ads_rewarded": "8c93fbfa8332af76",
       "backup_ads_rewarded": "ca-app-pub-3810129401881131/5479844119",
       "rewarded_counter": 1
